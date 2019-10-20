5d32323e412cfeb0b69aaca20b709e1c00697bed
桑宏锐 <sanghongrui@qq.com>
2019-10-20 21:06:09 +0800

    1.add feature: add starting scripts, chrome.sh

---
+++ launch/chrome.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+google-chrome http://localhost:3001 --start-fullscreen --incognito
--- launch/robot.sh
+++ launch/robot.sh
@@ -10,11 +10,15 @@ echo "starting serialport ..."
 gnome-terminal -x bash -c "zsh ./eyes.sh; exec bash;"
 sleep 3
 
-echo "starting npm ..."
-gnome-terminal -x bash -c "zsh ./website.sh; exec bash;"
-sleep 30
-
 echo "starting  proxy ..."
 gnome-terminal -x bash -c "zsh ./proxy.sh; exec bash;"
 sleep 3
+
+echo "starting npm ..."
+gnome-terminal -x bash -c "zsh ./website.sh; exec bash;"
+sleep 10
+
+echo "starting  chrome ..."
+gnome-terminal -x bash -c "zsh ./chrome.sh; exec bash;"
+sleep 60
 
